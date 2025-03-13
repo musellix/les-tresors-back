a6f20c4ae71407add25c38fff958f91c5a62c2d7
xavier.muselli <xavier.muselli@sii.fr>
2025-03-13 17:22:20 +0100

    feat(user): user resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig)],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
