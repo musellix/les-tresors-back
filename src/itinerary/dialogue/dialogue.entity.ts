424d0eb26010f718d819e5a45539a83c506b5c86
Musellix <musellix@gmail.com>
2025-03-20 23:09:11 +0100

    feat(itinerary): dialogue entity

---
+++ src/itinerary/dialogue/dialogue.entity.ts
@@ -0,0 +1,19 @@
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, Unique, ManyToOne, OneToMany } from 'typeorm';
+import { Itinerary } from '../itinerary/itinerary.entity';
+import { Step } from '../step/step.entity';
+
+@Entity()
+export class Dialogue {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => Step, (step) => step.dialogues)
+  step: Step;
+
+  @Column()
+  character: string;
+
+  @Column()
+  replica: string;
+
+}
