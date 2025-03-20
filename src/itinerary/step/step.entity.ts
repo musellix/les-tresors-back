73828435f3362f390d3814e80531d1dc75052c52
Musellix <musellix@gmail.com>
2025-03-20 23:09:00 +0100

    feat(itinerary): step entity

---
+++ src/itinerary/step/step.entity.ts
@@ -0,0 +1,18 @@
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, OneToMany } from 'typeorm';
+import { Itinerary } from '../itinerary/itinerary.entity';
+import { Dialogue } from '../dialogue/dialogue.entity';
+
+@Entity()
+export class Step {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => Itinerary, (itinerary) => itinerary.steps)
+  itinerary: Itinerary;
+
+  @Column()
+  title: string;
+
+  @OneToMany(() => Dialogue, (dialogue) => dialogue.step)
+  dialogues: Dialogue[]
+}
