4ec8f811094a94836d17550ce04726cacccbd300
xavier.muselli <xavier.muselli@sii.fr>
2025-03-20 13:03:02 +0100

    feat(itinerary): step controller

--- src/itinerary/itinerary.module.ts
+++ src/itinerary/itinerary.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ItineraryController } from './itinerary/itinerary.controller';
+import { StepController } from './step/step.controller';
 
 @Module({
-  controllers: [ItineraryController]
+  controllers: [ItineraryController, StepController]
 })
 export class ItineraryModule {}
