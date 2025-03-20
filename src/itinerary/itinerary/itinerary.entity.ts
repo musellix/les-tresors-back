345890940e02a7e7c1fa9f14d9c18b3436cd4804
Musellix <musellix@gmail.com>
2025-03-20 23:08:51 +0100

    feat(itinerary): itinerary entity

---
+++ src/itinerary/itinerary/itinerary.entity.ts
@@ -0,0 +1,38 @@
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, Unique, OneToMany } from 'typeorm';
+import { Step } from '../step/step.entity';
+
+export enum CacheType {
+  MULTICACHE = 'Multicache',
+  TRADITIONAL = 'Traditionellle',
+}
+
+
+@Entity()
+export class Itinerary {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true})
+  title: string;
+
+  @Column({ nullable: true })
+  photoUrl?: string;
+
+  @Column()
+  theme: string;
+
+  @Column({ type: 'text' })
+  typeOfCache: string;
+
+  @Column({ type: 'int', default: 1 })
+  difficulty: number;
+
+  @Column()
+  duration: string;
+
+  @Column()
+  accessibility: string;
+
+  @OneToMany(() => Step, (step) => step.itinerary)
+  steps: Step[]
+}
