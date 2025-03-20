ba2cd5da1b345fdfdc39920ad3b957e9662b1a80
xavier.muselli <xavier.muselli@sii.fr>
2025-03-20 12:54:29 +0100

    feat(itinerary): itinerary module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { typeOrmConfig } from './config/typeorm.config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { ItineraryModule } from './itinerary/itinerary.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), UserModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), UserModule, ItineraryModule],
   controllers: [AppController],
   providers: [AppService],
 })
