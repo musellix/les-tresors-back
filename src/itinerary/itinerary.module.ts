48a0f9a94be420a7fee32355639aafd1d9d53320
Xavier Muselli <xavier.muselli@sii.fr>
2025-03-28 14:58:53 +0100

    fix(itinerary): itinerary module - add imports for typeOrm

--- src/itinerary/itinerary.module.ts
+++ src/itinerary/itinerary.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ItineraryController } from './itinerary/itinerary.controller';
 import { StepController } from './step/step.controller';
 import { DialogueController } from './dialogue/dialogue.controller';
 import { ItineraryService } from './itinerary/itinerary.service';
 import { DialogueService } from './dialogue/dialogue.service';
+import { Step } from './step/step.entity';
+import { Dialogue } from './dialogue/dialogue.entity';
+import { Itinerary } from './itinerary/itinerary.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Itinerary, Dialogue, Step])],
   controllers: [ItineraryController, StepController, DialogueController],
   providers: [ItineraryService, DialogueService]
 })
