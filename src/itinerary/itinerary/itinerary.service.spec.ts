a8992defac4e5f706f78694772bac723b6147cfa
Xavier Muselli <xavier.muselli@sii.fr>
2025-03-31 12:45:23 +0200

    test(itinerary): itinerary service

--- src/itinerary/itinerary/itinerary.service.spec.ts
+++ src/itinerary/itinerary/itinerary.service.spec.ts
@@ -1,15 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ItineraryService } from './itinerary.service';
+import { Itinerary } from './itinerary.entity';
+import { Repository } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('ItineraryService', () => {
   let service: ItineraryService;
+  let itineraryRepository: Repository<Itinerary>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ItineraryService],
+      providers: [
+        ItineraryService,
+        {
+          provide: getRepositoryToken(Itinerary),
+          useValue: {
+            save: jest.fn(),
+          },
+        }
+      ],
     }).compile();
 
     service = module.get<ItineraryService>(ItineraryService);
+    itineraryRepository = module.get<Repository<Itinerary>>(getRepositoryToken(Itinerary));
   });
 
   it('should be defined', () => {
