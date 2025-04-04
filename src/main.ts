ed19442871d33cc0f76bd35d5dea2f3229a589ae
Xavier Muselli <xavier.muselli@sii.fr>
2025-04-04 16:25:35 +0200

    chore(openApi): configuration de Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  // Configuration de Swagger (OpenAPI)
+  const config = new DocumentBuilder()
+    .setTitle(`Les tresors d'Ille et Vilaine API`)
+    .setDescription('Documentation de l\'API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
