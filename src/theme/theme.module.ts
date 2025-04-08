64cc59b4458dd014b24f2da5e4ff33bafda8c06f
Xavier Muselli <xavier.muselli@sii.fr>
2025-04-08 17:23:07 +0200

    feat(theme): theme component II

--- src/theme/theme.module.ts
+++ src/theme/theme.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ThemeController } from './theme.controller';
 import { ThemeService } from './theme.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Theme } from './theme.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Theme])],
   controllers: [ThemeController],
   providers: [ThemeService]
 })
